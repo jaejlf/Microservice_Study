6924cfd8277ace4bf0df3dd930cca33c8800d67a
jaejeong <78673570+jaejlf@users.noreply.github.com>
2023-01-08 01:25:06 +0900

    user 마이크로서비스 연결

--- micro-shop/apigateway-service/src/main/java/msa/apigatewayservice/ApigatewayServiceApplication.java
+++ micro-shop/apigateway-service/src/main/java/msa/apigatewayservice/ApigatewayServiceApplication.java
@@ -2,7 +2,9 @@ package msa.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
--- micro-shop/user-service/src/main/java/msa/userservice/controller/HealthCheckController.java
+++ micro-shop/user-service/src/main/java/msa/userservice/controller/HealthCheckController.java
@@ -1,14 +1,12 @@
 package msa.userservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
 public class HealthCheckController {
 
-    @GetMapping("/check")
+    @GetMapping("/health")
     public String healthCheck() {
         return "OK";
     }
