34bb4ab980b3740e1a305b377ac156473318ac89
Jaejeong Sohn <jaejlf@naver.com>
2023-01-27 16:40:08 +0900

    프로젝트 초기 설정

---
+++ async/AsyncOne/src/main/java/study/asyncone/AsyncOneApplication.java
@@ -0,0 +1,13 @@
+package study.asyncone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncOneApplication.class, args);
+	}
+
+}
---
+++ async/AsyncOne/src/test/java/study/asyncone/AsyncOneApplicationTests.java
@@ -0,0 +1,13 @@
+package study.asyncone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
